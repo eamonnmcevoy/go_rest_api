ed90516b7030a2b2585aab38169efdbbec9478b9
Eamonn McEvoy <eamonnmcevoy@outlook.com>
2017-03-05 21:25:01 +1100

    route to user by username

--- user/routes.go
+++ user/routes.go
@@ -15,9 +15,10 @@ type userRouter struct {
 func NewUserRouter(session *mgo.Session, router *mux.Router) *mux.Router {
   u := userRouter{NewUserService(session)}
 
-  router.HandleFunc("/create", u.createUserHandler)
-  router.HandleFunc("/profile", validate(u.getProfileHandler))
-  router.HandleFunc("/login", u.loginHandler)
+  router.HandleFunc("/", u.createUserHandler).Methods("PUT")
+  router.HandleFunc("/", validate(u.profileHandler)).Methods("GET")
+  router.HandleFunc("/{username}", u.getUserHandler).Methods("GET")
+  router.HandleFunc("/login", u.loginHandler).Methods("POST")
   return router
 }
 
@@ -37,7 +38,7 @@ func(u* userRouter) createUserHandler(w http.ResponseWriter, r *http.Request) {
   response.Json(w, http.StatusOK, err)
 }
 
-func(u* userRouter) getProfileHandler(w http.ResponseWriter, r *http.Request) {
+func(u* userRouter) profileHandler(w http.ResponseWriter, r *http.Request) {
   claims := r.Context().Value(contextKeyAuthtoken).(Claims)
   username := claims.Username
 
@@ -49,6 +50,20 @@ func(u* userRouter) getProfileHandler(w http.ResponseWriter, r *http.Request) {
 
   response.Json(w, http.StatusOK, user)
 }
+
+func(u* userRouter) getUserHandler(w http.ResponseWriter, r *http.Request) {
+  vars := mux.Vars(r)
+  username := vars["username"]
+  
+  err, user := u.service.getUserByUsername(username)
+  if err != nil {
+    response.Error(w, http.StatusInternalServerError, err.Error())
+    return
+  }
+
+  response.Json(w, http.StatusOK, user)
+}
+
 
 func(u *userRouter) loginHandler(w http.ResponseWriter, r *http.Request) {
   err, credentials := decodeCredentials(r)
@@ -65,6 +80,13 @@ func(u *userRouter) loginHandler(w http.ResponseWriter, r *http.Request) {
   } else {
     response.Error(w, http.StatusInternalServerError, "Incorrect password")
   }
+}
+
+func decodeUser(r *http.Request) (error,User) {
+  var u User
+  decoder := json.NewDecoder(r.Body)
+  err := decoder.Decode(&u)
+  return err, u
 }
 
 func decodeCredentials(r *http.Request) (error,Credentials) {
